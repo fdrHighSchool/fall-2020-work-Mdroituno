ef495f0c93ee073f7706c9eab67a4b7531478615
Mdroituno <unconfigured@null.spigotmc.org>
2021-01-09 23:16:48 -0500

    instances

--- 13-student/Student.java
+++ 13-student/Student.java
@@ -1,37 +1,47 @@
 public class Student {
-    // instance varaibles
+    // instance variables
  
     private String name;
     private String osis;
     private int[] grades;
 
     // constructor
-
-
+    public Student(String n, String o){
+        this.name = n;
+        this.osis = o;
+        grades = new int[10];
+    }//end constructor method
+    
     // other methods
 
 
     public String getName(){
 
+        return "";
     } //end of getName method
 
 
     public String getOsis(){
 
+        return "";
     } //end of getOsis method
 
-    public void setName(){
-
+    public void setName(String name){
+        this.name = name;
+       
     } //end of setName method
 
-    public void addGrades(int arr) {
+    public void addGrade(int arr) {
 
 
-    } //end addGrades()
+    } //end addGrade method
 
     public double calculateAverage(){
 
+        return 0.0;
     } //end of calculateAverage method
 
-
+    public String toString(){
+        return "Student: " + this.name + " OSIS: " + this.osis;
+    }
 }//end class
--- 13-student/StudentDriver.java
+++ 13-student/StudentDriver.java
@@ -1,3 +1,17 @@
 public class StudentDriver{
-    
+    public static void main(String[] args) {
+        Student student1 = new Student("Hans", "123456789");
+        System.out.println(student1);
+        student1.setName("Joshua");
+        System.out.println(student1);
+
+        Student student2 = new Student("Doe", "987654321");
+        System.out.println(student2);
+        student2.setName("John");
+        System.out.println(student2);
+
+        Student student3 = new Student("Bobby", "052879527");
+        System.out.println(student3);
+
+    }//end main method
 }
