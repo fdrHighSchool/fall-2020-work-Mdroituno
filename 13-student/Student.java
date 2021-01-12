042b1b726381062767d259ee338ef6ce6ac08af9
Mdroituno <unconfigured@null.spigotmc.org>
2021-01-12 12:41:16 -0500

    updated

--- 13-student/Student.java
+++ 13-student/Student.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Student {
     // instance variables
  
@@ -17,28 +19,48 @@ public class Student {
 
     public String getName(){
 
-        return "";
+        return this.name;
     } //end of getName method
 
 
     public String getOsis(){
 
-        return "";
+        return this.osis;
     } //end of getOsis method
+
+    public void displayGrades() {
+        System.out.println(Arrays.toString(this.grades));;
+    }
 
     public void setName(String name){
         this.name = name;
        
     } //end of setName method
-
-    public void addGrade(int arr) {
-
-
-    } //end addGrade method
+    
+    public void fillArray() {
+        for(int i = 0; i < this.grades.length; i++){
+         this.grades[i] = (int) (Math.random() * 46 + 55);
+        }//end col for loop
+    } //end fillArray method
+    
+    public void addGrade (int grade){
+        int i = 0;
+        while (this.grades[i] != 0) {
+            i++;
+    }
+        this.grades[i] = grade;
+    }
 
     public double calculateAverage(){
-
-        return 0.0;
+        double sum = 0;
+        int count = 0;
+        for(int i = 0; i < this.grades.length; i++){
+            if(this.grades[i] != 0){
+                sum += this.grades[i];
+                count++;
+            }//end if statement
+        }// end for loop
+        return sum/count;
     } //end of calculateAverage method
 
     public String toString(){
--- 13-student/StudentDriver.java
+++ 13-student/StudentDriver.java
@@ -4,14 +4,28 @@ public class StudentDriver{
         System.out.println(student1);
         student1.setName("Joshua");
         System.out.println(student1);
+        student1.addGrade(100);
+        student1.displayGrades();
+        student1.addGrade(85);
+        student1.displayGrades();
 
         Student student2 = new Student("Doe", "987654321");
         System.out.println(student2);
         student2.setName("John");
         System.out.println(student2);
+        student2.addGrade((95));
+        student2.displayGrades();
 
         Student student3 = new Student("Bobby", "052879527");
         System.out.println(student3);
+        student3.fillArray();
+        student3.displayGrades();
+
+        System.out.println(student1.calculateAverage());
+        System.out.println(student2.calculateAverage());
+        System.out.println(student3.calculateAverage());
+
+
 
     }//end main method
 }
